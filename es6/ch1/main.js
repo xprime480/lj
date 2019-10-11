c70b50cc48d7a189d5f01f05cad2768f1d2b362d
Mike Davis <xprime480@gmail.com>
2019-10-11 12:04:01 -0700

    Configure eslint task.
        Ignore ch1 inputs.

--- es6/ch1/main.js
+++ es6/ch1/main.js
@@ -1,3 +1,5 @@
+/* eslint no-undef: "off" */
+
 $(document).ready(function () {
     'use strict';
     paper.install(window);
--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,36 @@
-const gulp = require('gulp');
-const babel = require('gulp-babel');
+const gulp   = require('gulp');
+const babel  = require('gulp-babel');
+const eslint = require('gulp-eslint');
+
+function lintTask(cb) {
+    gulp.src("es6/**/*.js")
+        .pipe(eslint())
+        .pipe(eslint.format());
+
+    cb();
+}
 
 function jsTask(cb) {
     gulp.src("es6/**/*.js")
-	.pipe(babel())
-	.pipe(gulp.dest("dist"));
+        .pipe(babel())
+        .pipe(gulp.dest("dist"));
 
     cb();
 }
 
 function cssTask(cb) {
     gulp.src("es6/**/*.css")
-	.pipe(gulp.dest("dist"));
+        .pipe(gulp.dest("dist"));
 
     cb();
 }
 
 function htmlTask(cb) {
     gulp.src("es6/**/*.html")
-	.pipe(gulp.dest("dist"));
+        .pipe(gulp.dest("dist"));
 
     cb();
 }
 
-exports.default = gulp.parallel(jsTask, cssTask, htmlTask);
+exports.default = gulp.series(lintTask,
+			      gulp.parallel(jsTask, cssTask, htmlTask));
